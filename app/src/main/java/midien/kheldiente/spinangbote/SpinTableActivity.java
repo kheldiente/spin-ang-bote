9cc46a3d74f51847726ee1cfacf5ff8acd0ee315
Michael Diente <kheldiente@gmail.com>
2018-01-27 19:05:21 +0800

    Added on bottle stopped listener

--- app/src/main/java/midien/kheldiente/spinangbote/SpinTableActivity.java
+++ app/src/main/java/midien/kheldiente/spinangbote/SpinTableActivity.java
@@ -2,12 +2,33 @@ package midien.kheldiente.spinangbote;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
-public class SpinTableActivity extends AppCompatActivity {
+import midien.kheldiente.spinangbote.customviews.SpinTable;
+
+public class SpinTableActivity extends AppCompatActivity implements SpinTable.OnBottleStoppedListener {
+
+    private static final String TAG = SpinTableActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spin_table);
+
+        final SpinTable spinTable = findViewById(R.id.spin_table);
+        spinTable.setListener(this);
+        findViewById(R.id.btn_spin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                spinTable.spinBottle();
+            }
+        });
+    }
+
+    @Override
+    public void onBottleStopped() {
+        Log.d(TAG, "Bottle stopped");
     }
 }
