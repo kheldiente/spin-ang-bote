e4f31a7e2c38098b01588ced7ca5375b5b733749
Michael Diente <kheldiente@gmail.com>
2018-02-09 18:59:17 +0800

    Enable view inflation using xml for TruthDareDialog
    - Use addContentView method to inflate xml to TruthDareDialog

--- app/src/main/java/midien/kheldiente/spinangbote/views/TruthDareDialog.java
+++ app/src/main/java/midien/kheldiente/spinangbote/views/TruthDareDialog.java
@@ -4,19 +4,20 @@ import android.animation.Animator;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.drawable.ColorDrawable;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.util.AttributeSet;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.AccelerateInterpolator;
+import android.widget.Button;
+import android.widget.TextView;
 
 import midien.kheldiente.spinangbote.R;
 
@@ -25,6 +26,8 @@ public class TruthDareDialog extends DialogFragment {
     private String mPlayer = "";
 
     private TruthDareCard mTruthCard;
+    private TextView mTruthDareTxt;
+    private Button mDoneBtn;
 
     public static TruthDareDialog newInstance() {
         TruthDareDialog f = new TruthDareDialog();
@@ -41,8 +44,29 @@ public class TruthDareDialog extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         getDialog().getWindow().setBackgroundDrawableResource(android.R.color.transparent);
         mTruthCard = new TruthDareCard(getContext());
+
+        setFormWidgets();
         return mTruthCard;
     }
+
+    private void setFormWidgets() {
+        // Inflate your own custom layout
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.dialog_truthdare, null);
+        mTruthDareTxt = view.findViewById(R.id.truthdare_msg);
+        mDoneBtn = view.findViewById(R.id.btn_done);
+
+        mTruthDareTxt.setText(getString(R.string.truth_or_dare_msg, mPlayer));
+        mDoneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+
+        // Add view to TruthCard view
+        addContentView(view);
+    }
+
 
     @Override
     public void onStart() {
@@ -59,20 +83,21 @@ public class TruthDareDialog extends DialogFragment {
         mPlayer = player;
     }
 
-    private class TruthDareCard extends View implements ValueAnimator.AnimatorUpdateListener {
+    public void addContentView(View view) {
+        mTruthCard.addView(view);
+    }
 
-        private Paint mConsTxtPaint;
 
-        private float mTextX, mTextY;
+    public class TruthDareCard extends ViewGroup implements ValueAnimator.AnimatorUpdateListener, ValueAnimator.AnimatorListener {
 
         private static final int FLIPS = 2;
+
+        int deviceWidth;
 
         // Animator
         private ValueAnimator mAnimator;
         private float mAnimatingFraction;
 
-        private static final int DESIRED_WIDTH = 200;
-        private static final int DESIRED_HEIGHT = 1000;
 
         public TruthDareCard(Context context) {
             super(context);
@@ -85,20 +110,19 @@ public class TruthDareDialog extends DialogFragment {
         }
 
         private void init() {
-            setBackgroundResource(R.drawable.floor_bg);
+            final Display display = ((WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+            Point deviceDisplay = new Point();
+            display.getSize(deviceDisplay);
+            deviceWidth = deviceDisplay.x;
 
-            mConsTxtPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-            mConsTxtPaint.setColor(Color.BLACK);
-            mConsTxtPaint.setTextAlign(Paint.Align.CENTER);
-            mConsTxtPaint.setStyle(Paint.Style.FILL_AND_STROKE);
-            mConsTxtPaint.setFakeBoldText(true);
-            mConsTxtPaint.setTextSize(50);
+            setBackgroundResource(R.drawable.floor_bg);
 
             // Setup value animator
             mAnimator = new ValueAnimator();
             mAnimator.setInterpolator(new AccelerateInterpolator());
             mAnimator.setDuration(200);
             mAnimator.addUpdateListener(this);
+            mAnimator.addListener(this);
             mAnimatingFraction = 0f;
 
             setScaleX(mAnimatingFraction);
@@ -118,61 +142,64 @@ public class TruthDareDialog extends DialogFragment {
         }
 
         @Override
+        protected void onLayout(boolean changed, int l, int t, int r, int b) {
+            // Notice:
+            // This logic only works on LinearLayout. Layouts that are relative and use constraints
+            // are not supported
+            //
+            // Only get the first index. ONLY 1 child is allowed to add
+            final int count = getChildCount();
+            if(count == 0)
+                return;
+
+            // Get the available size of child view
+            final int childLeft = this.getPaddingLeft();
+            final int childTop = this.getPaddingTop();
+            final int childRight = this.getMeasuredWidth() - this.getPaddingRight();
+            final int childBottom = this.getMeasuredHeight() - this.getPaddingBottom();
+
+            View child = getChildAt(0);
+
+            if (child.getVisibility() == GONE)
+                return;
+
+            // Do the layout
+            child.layout(childLeft, childTop, childRight, childBottom);
+
+        }
+
+        @Override
         protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-            int widthMode = MeasureSpec.getMode(widthMeasureSpec);
-            int widthSize = MeasureSpec.getSize(widthMeasureSpec);
-            int heightMode = MeasureSpec.getMode(heightMeasureSpec);
-            int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+            int count = getChildCount();
+            if(count == 0)
+                return;
 
-            int width, height;
+            // Measurement will ultimately be computing these values.
+            int maxWidth = 0;
+            int childState = 0;
 
-            switch(widthMode) {
-                case MeasureSpec.EXACTLY:
-                    // Must be this size
-                    width = widthSize;
-                    break;
-                case MeasureSpec.AT_MOST:
-                    // Can't be bigger than..
-                    width = Math.min(DESIRED_WIDTH, widthSize);
-                    break;
-                default:
-                    // Be desired width
-                    width = DESIRED_WIDTH;
-                    break;
-            }
+            final View child = getChildAt(0);
 
-            switch(heightMode) {
-                case MeasureSpec.EXACTLY:
-                    // Must the this size
-                    height = heightSize;
-                    break;
-                case MeasureSpec.AT_MOST:
-                    // Can't be bigger than..
-                    height = Math.min(DESIRED_HEIGHT, heightSize);
-                    break;
-                default:
-                    // Be desired width
-                    height = DESIRED_HEIGHT;
-                    break;
-            }
+            if (child.getVisibility() == GONE)
+                return;
 
-            // Must call to set height and width
-            setMeasuredDimension(width, height);
+            // Measure the child.
+            measureChild(child, widthMeasureSpec, heightMeasureSpec);
+            maxWidth += Math.max(maxWidth, child.getMeasuredWidth());
+
+            setMeasuredDimension(resolveSizeAndState(maxWidth, widthMeasureSpec, childState), child.getMeasuredHeight());
         }
 
         @Override
         protected void onSizeChanged(int w, int h, int oldw, int oldh) {
             super.onSizeChanged(w, h, oldw, oldh);
-
-            mTextX = w / 2;
-            mTextY = h / 4;
         }
 
         @Override
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
 
-            canvas.drawText(mPlayer, mTextX, mTextY, mConsTxtPaint);
+            // canvas.drawText(getResources().getString(R.string.truth_or_dare_msg), mTextX, mTextY, mConsTxtPaint);
             // Scale y to gradually scale width
             setScaleX(mAnimatingFraction);
         }
@@ -184,5 +211,17 @@ public class TruthDareDialog extends DialogFragment {
             // Must call this to redraw view
             invalidate();
         }
+
+        @Override
+        public void onAnimationStart(Animator animator) {}
+
+        @Override
+        public void onAnimationEnd(Animator animator) {}
+
+        @Override
+        public void onAnimationCancel(Animator animator) {}
+
+        @Override
+        public void onAnimationRepeat(Animator animator) {}
     }
 }
